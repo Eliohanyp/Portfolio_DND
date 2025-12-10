71febb0389502423933c6ccaf6a30e4310727771
Eliohan Yamauti Poaiti <87502469+Eliohanyp@users.noreply.github.com>
2025-12-10 15:56:27 -0300

    Add files via upload

---
+++ video-config.js
@@ -0,0 +1,15 @@
+// Configuração das URLs dos vídeos
+// Para Cloudinary, use o formato: https://res.cloudinary.com/[cloud_name]/video/upload/[public_id].mp4
+// Substitua [cloud_name] pelo seu cloud name e [public_id] pelo ID do vídeo
+
+const videoConfig = {
+    // Vídeo 1 (03.mp4)
+    // Formato correto: https://res.cloudinary.com/dzs0rutgj/video/upload/03_o00xu1.mp4
+    video1: "https://res.cloudinary.com/dzs0rutgj/video/upload/03_o00xu1.mp4",
+
+};
+
+// Se você quiser usar vídeos do YouTube ou Vimeo, use o formato embed:
+// YouTube: "https://www.youtube.com/embed/VIDEO_ID?autoplay=1&mute=1&loop=1&playlist=VIDEO_ID"
+// Vimeo: "https://player.vimeo.com/video/VIDEO_ID?autoplay=1&mute=1&loop=1"
+
